05668c35c73eb5f079f495df1be6415d7a7c6ccf
weisslip <roeeweisslip@gmail.com>
2020-01-07 21:10:59 +0200

    simple commit. changed practically nothing.

--- Boost_Echo_Client/include/StompEncoderDecoder.h
+++ Boost_Echo_Client/include/StompEncoderDecoder.h
@@ -1,15 +1,9 @@
-//
-// Created by weisslip@wincs.cs.bgu.ac.il on 07/01/2020.
-//
-
 #ifndef BOOST_ECHO_CLIENT_STOMPENCODERDECODER_H
 #define BOOST_ECHO_CLIENT_STOMPENCODERDECODER_H
 
 
 class StompEncoderDecoder {
-private:
-    byte[] bytes = byte[1 << 10];
-    int len = 0;
+
 };
 
 
