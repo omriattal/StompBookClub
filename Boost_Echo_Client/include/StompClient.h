1a2549790a3ac077f4b46693e8dc6db4d17d68f5
weisslip <roeeweisslip@gmail.com>
2020-01-07 19:55:11 +0200

    created StompClient .cpp and .h

---
+++ Boost_Echo_Client/include/StompClient.h
@@ -0,0 +1,14 @@
+//
+// Created by weisslip@wincs.cs.bgu.ac.il on 07/01/2020.
+//
+
+#ifndef BOOST_ECHO_CLIENT_STOMPCLIENT_H
+#define BOOST_ECHO_CLIENT_STOMPCLIENT_H
+
+
+class StompClient {
+
+};
+
+
+#endif //BOOST_ECHO_CLIENT_STOMPCLIENT_H
