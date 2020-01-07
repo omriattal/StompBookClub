cde7a417277b3a4c849837366cf2c3b620188a2d
weisslip <roeeweisslip@gmail.com>
2020-01-07 18:35:24 +0200

    implemented main function

--- spl-net/src/main/java/bgu/spl/net/impl/stomp/StompServer.java
+++ spl-net/src/main/java/bgu/spl/net/impl/stomp/StompServer.java
@@ -1,7 +1,18 @@
 package bgu.spl.net.impl.stomp;
 
+import bgu.spl.net.srv.Reactor;
+import bgu.spl.net.srv.Server;
+
 public class StompServer {
 
 	public static void main(String[] args) {
+		Server<StompFrame> server;
+		if(args[0].toLowerCase().equals("reactor")){
+			server = Server.reactor(new Integer(args[1]), 7777, StompMessagingProtocolImpl::new, StompEncoderDecoder::new);
+		}
+		else {
+			server = Server.threadPerClient(7777, StompMessagingProtocolImpl::new, StompEncoderDecoder::new);
+		}
+		server.serve();
 	}
 }
