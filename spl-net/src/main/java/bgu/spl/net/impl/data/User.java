6827997456d3720cc69a69a4bad74295204fdcff
weisslip <roeeweisslip@gmail.com>
2020-01-09 20:02:09 +0200

    Removed unnecessary methods and fields in User. There are no longer complex data structures in User.

--- spl-net/src/main/java/bgu/spl/net/impl/data/User.java
+++ spl-net/src/main/java/bgu/spl/net/impl/data/User.java
@@ -1,19 +1,15 @@
 package bgu.spl.net.impl.data;
 
-import java.util.HashMap;
-
 public class User {
 	public final String name;
 	public final String password;
 	private int connectionId;
 	private boolean isLoggedIn = false;
-	private HashMap<Integer, String> subscriptionMap;
 
 	public User(int connectionId, String name, String password) {
 		this.connectionId = connectionId;
 		this.name = name;
 		this.password = password;
-		subscriptionMap = new HashMap<>();
 	}
 
 	public boolean isLoggedIn() {
@@ -36,25 +32,6 @@ public class User {
 		this.connectionId = connectionId;
 	}
 
-	public void subscribe(int subId, String topic) {
-		subscriptionMap.put(subId, topic);
-	}
 
-	public HashMap<Integer, String> getSubscriptionMap() {
-		return subscriptionMap;
-	}
-
-	public String getUserName() {
-		return name;
-	}
-
-
-	public void unsubscribe(int subId) {
-		subscriptionMap.remove(subId);
-	}
-
-	public String getTopic(int subId) {
-		return subscriptionMap.get(subId);
-	}
 
 }
