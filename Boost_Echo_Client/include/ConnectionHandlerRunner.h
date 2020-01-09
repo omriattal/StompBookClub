f46f1d0905f43dc5db1a81821f5856c3b783fc23
weisslip <roeeweisslip@gmail.com>
2020-01-09 20:28:04 +0200

    stam commit

---
+++ Boost_Echo_Client/include/ConnectionHandlerRunner.h
@@ -0,0 +1,19 @@
+//
+// Created by weisslip@wincs.cs.bgu.ac.il on 09/01/2020.
+//
+
+#ifndef BOOST_ECHO_CLIENT_CONNECTIONHANDLERRUNNER_H
+#define BOOST_ECHO_CLIENT_CONNECTIONHANDLERRUNNER_H
+
+#include "connectionHandler.h"
+#include "StompFrame.h"
+
+class ConnectionHandlerRunner {
+private:
+
+public:
+    void run();
+};
+
+
+#endif //BOOST_ECHO_CLIENT_CONNECTIONHANDLERRUNNER_H
---
+++ Boost_Echo_Client/src/ConnectionHandlerRunner.cpp
@@ -0,0 +1,5 @@
+//
+// Created by weisslip@wincs.cs.bgu.ac.il on 09/01/2020.
+//
+
+#include "ConnectionHandlerRunner.h"
